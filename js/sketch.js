36c4aaec029c193cc933c4d04b0d1fa2ba07fe7f
Mikael Kotkavuori <mikael.kotkavuori@gmail.com>
2017-12-20 19:34:06 +0200

    Mouseover works on all bars, added title, added canvas to a div element.

--- js/sketch.js
+++ js/sketch.js
@@ -1,108 +1,105 @@
 var canvas;
 let canvasHeight = 300;
 let canvasWidth = 600;
-let arr = [];
-let arrB = [];
-let touched = false;
+let bars = [];
+let len = 5;
+
+
 const testH = document.getElementById('test');
 
 function setup() {
   canvas = createCanvas(canvasWidth, canvasHeight);
   background(100, 200, 200);
+  canvas.parent('canvas');
   noStroke();
   fill(100, 100, 250);
 
+  let margin = 40;
   let rectH = canvasHeight - 50;
-  let rectW = canvasWidth / 12;
-  let margin = 30;
+  let rectW = (canvasWidth / len) - ((margin*(len+1))/len);
   let graphNum = 5;
   for (var i = 0; i < graphNum; i++) {
     let height = rectH - random(150);
     let xStart = (margin * (i + 1) + (rectW * i));
     let yStart = canvasHeight - height;
-    //    rect(xStart, yStart, rectW, height);
-    let obj = {
+
+    let bar = {
       xStart: xStart,
       xEnd: xStart + rectW,
       yStart: yStart,
       yEnd: canvasHeight,
       height: height,
-      width: rectW
+      width: rectW,
+      oXStart: xStart,
+      oXEnd: xStart + rectW,
+      oYStart: yStart,
+      oYEnd: canvasHeight,
+      oHeight: height,
+      oWidth: rectW,
+      touched: false,
+      title: ""
     };
-    let obj2 = {
-      xStart: xStart,
-      xEnd: xStart + rectW,
-      yStart: yStart,
-      yEnd: canvasHeight,
-      height: height,
-      width: rectW
-    };
-    arr.push(obj);
-    arrB.push(obj2);
+    bars.push(bar);
   }
+  bars[0].title = " ding ";
+  bars[1].title = " dong ";
+  bars[2].title = " the witch ";
+  bars[3].title = " is ";
+  bars[4].title = " dead ";
 }
 
 
 function draw() {
   fill(100, 100, 250);
 
-for(var i = 0; i < arrB.length; i++) {
-  rect(arrB[i].xStart, arrB[i].yStart, arrB[i].width, arrB[i].height);
-}
-  //
-  // rect(arrB[0].xStart, arrB[0].yStart, arrB[0].width, arrB[0].height);
-  // rect(arrB[1].xStart, arrB[1].yStart, arrB[1].width, arrB[1].height);
-  // rect(arrB[2].xStart, arrB[2].yStart, arrB[2].width, arrB[2].height);
-  // rect(arrB[3].xStart, arrB[3].yStart, arrB[3].width, arrB[3].height);
-  // rect(arrB[4].xStart, arrB[4].yStart, arrB[4].width, arrB[4].height);
+  for (let bar of bars) {
+    fill(100, 100, 250);
+    rect(bar.xStart, bar.yStart, bar.width, bar.height);
+    textAlign(CENTER);
+
+    fill(0);
+    text(bar.title, (bar.xStart + bar.width / 2), canvasHeight - 40, 0);
+  }
+
 
   handleMouseover();
+
+
 }
 
 function handleMouseover() {
-  let obj = arrB[0];
-  let obj2 = arr[0];
-  if (checkBounds(obj) && obj.width < obj2.width + 10) {
-    obj.xStart -= 1;
-    obj.width += 1;
-    obj.yStart -= 2;
-    obj.height += 2;
-    touched = true;
-  } else {
-    if (!checkBounds(obj)) {
-      obj.xStart = obj2.xStart;
-      obj.width = obj2.width;
-      obj.yStart = obj2.yStart;
-      obj.height = obj2.height;
-      if (touched) {
-        fill(100, 200, 200);
-        rect(obj.xStart-15, obj.yStart-25, obj.width + 15, obj.height + 25);
-        touched = false;
+  for (let bar of bars) {
+    if (checkBounds(bar) && bar.width < bar.oWidth + 10) {
+      bar.xStart -= 1;
+      bar.width += 1;
+      bar.yStart -= 4;
+      bar.height += 4;
+      bar.touched = true;
+    }
+    else {
+      if (!checkBounds(bar)) {
+        bar.xStart = bar.oXStart;
+        bar.width = bar.oWidth;
+        bar.yStart = bar.oYStart;
+        bar.height = bar.oHeight;
+
+        if (bar.touched) {
+          fill(100, 200, 200);
+          rect(bar.xStart - 15, bar.yStart - 50, bar.width + 30, bar.height + 50);
+          bar.touched = false;
+        }
       }
     }
   }
 }
 
+
 function mousePressed() {
-  if (checkBounds(arr[0])) {
-    console.log('ding');
-    testH.innerHTML += 'ding ';
-  }
-  if (checkBounds(arr[1])) {
-    console.log('dong');
-    testH.innerHTML += ' dong';
-  }
-  if (checkBounds(arr[2])) {
-    console.log('the witch');
-    testH.innerHTML += ' the witch';
-  }
-  if (checkBounds(arr[3])) {
-    console.log('is');
-    testH.innerHTML += ' is';
-  }
-  if (checkBounds(arr[4])) {
-    console.log('dead');
-    testH.innerHTML += ' dead';
+  for(let bar of bars) {
+    if(checkBounds(bar)) {
+      console.log(bar.title);
+      testH.innerHTML += bar.title;
+    }
   }
 }
 
